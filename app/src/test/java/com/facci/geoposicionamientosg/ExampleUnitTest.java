7d431e2b5353aa0cf34928429cb6761ea34cad12
guerrero596 <guerrero596@hotmail.com>
2016-09-13 12:08:14 -0500

    Trabajo
    Codigo de geoposicion

---
+++ app/src/test/java/com/facci/geoposicionamientosg/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.facci.geoposicionamientosg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
